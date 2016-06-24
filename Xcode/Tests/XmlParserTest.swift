fecfa0a6431f5ea7c9bf0e0109d2de64b75a89e8
Benjamin Werker <benjamin@widgetlabs.eu>
2016-06-24 13:15:43 +0200

    added xml config parser tests

--- Xcode/Tests/XmlParserTest.swift
+++ Xcode/Tests/XmlParserTest.swift
@@ -3,11 +3,12 @@ import XCTest
 import Foundation
 @testable import Webtrekk
 
-class XmlParserTest: XCTestCase {
+
+internal class XmlParserAutomaticTest: XCTestCase {
 
 	internal var configurationData: NSData = {
-		guard let configurationFile = NSBundle(forClass: XmlParserTest.self).URLForResource("DefaultConfig", withExtension: "xml") else {
-			fatalError("Cannot locate DefaultConfig.xml")
+		guard let configurationFile = NSBundle(forClass: XmlParserManualTest.self).URLForResource("ConfigAutomatic", withExtension: "xml") else {
+			fatalError("Cannot locate ConfigAutomatic.xml")
 		}
 		guard let configurationData = NSData(contentsOfURL: configurationFile) else {
 			fatalError("Cannot load Webtrekk configuration file '\(configurationFile)'")
@@ -16,12 +17,152 @@ class XmlParserTest: XCTestCase {
 	}()
 
 
-	func testParserInit() {
+	internal func testParserInit() {
 		do {
 			try XmlTrackerConfigurationParser().parse(xml: configurationData)
 		}
 		catch let error {
 			fatalError("Cannot Read Webtrekk configuration: \(error)")
 		}
+	}
+
+	internal func testParserConfig() {
+		let config: TrackerConfiguration
+		do {
+			config = try XmlTrackerConfigurationParser().parse(xml: configurationData)
+		}
+		catch let error {
+			fatalError("Cannot Read Webtrekk configuration: \(error)")
+		}
+		XCTAssertEqual(config.version, 1)
+		XCTAssertEqual(config.serverUrl.absoluteString, "https://q3.webtrekk.net")
+		XCTAssertEqual(config.webtrekkId, "289053685367929")
+		XCTAssertEqual(config.samplingRate, 0)
+		XCTAssertEqual(config.maximumSendDelay, 30)
+		XCTAssertEqual(config.sessionTimeoutInterval, 1800)
+		XCTAssertEqual(config.eventQueueLimit, 5000)
+		XCTAssertNotNil(config.configurationUpdateUrl)
+		if let url = config.configurationUpdateUrl {
+			XCTAssertEqual(url.absoluteString, "https://your.domain/webtrekk.xml")
+		}
+		XCTAssertEqual(config.automaticallyTracksAdvertisingId, true)
+		XCTAssertEqual(config.automaticallyTracksAppUpdates, true)
+		XCTAssertEqual(config.automaticallyTracksAppVersion, true)
+		XCTAssertEqual(config.automaticallyTracksConnectionType, true)
+		XCTAssertEqual(config.automaticallyTracksEventQueueSize, true)
+		XCTAssertEqual(config.automaticallyTracksInterfaceOrientation, true)
+
+		XCTAssertFalse(config.automaticallyTrackedPages.isEmpty)
+
+		for (index, page) in config.automaticallyTrackedPages.enumerate() {
+			XCTAssertNotNil(page.pageProperties.name)
+			guard index == 1 else {
+				continue
+			}
+			if let details = page.pageProperties.details {
+				XCTAssertTrue(details.count == 2)
+			}
+			if let groups = page.pageProperties.groups {
+				XCTAssertTrue(groups.count == 1)
+			}
+		}
+	}
+}
+
+
+internal class XmlParserManualTest: XCTestCase {
+
+	internal var configurationData: NSData = {
+		guard let configurationFile = NSBundle(forClass: XmlParserManualTest.self).URLForResource("ConfigManual", withExtension: "xml") else {
+			fatalError("Cannot locate ConfigManual.xml")
+		}
+		guard let configurationData = NSData(contentsOfURL: configurationFile) else {
+			fatalError("Cannot load Webtrekk configuration file '\(configurationFile)'")
+		}
+		return configurationData
+	}()
+
+
+	internal func testParserInit() {
+		do {
+			try XmlTrackerConfigurationParser().parse(xml: configurationData)
+		}
+		catch let error {
+			fatalError("Cannot Read Webtrekk configuration: \(error)")
+		}
+	}
+
+	internal func testParserConfig() {
+		let config: TrackerConfiguration
+		do {
+			config = try XmlTrackerConfigurationParser().parse(xml: configurationData)
+		}
+		catch let error {
+			fatalError("Cannot Read Webtrekk configuration: \(error)")
+		}
+		XCTAssertEqual(config.version, 1)
+		XCTAssertEqual(config.serverUrl.absoluteString, "https://q3.webtrekk.net")
+		XCTAssertEqual(config.webtrekkId, "289053685367929")
+		XCTAssertEqual(config.samplingRate, 0)
+		XCTAssertEqual(config.maximumSendDelay, 30)
+		XCTAssertEqual(config.sessionTimeoutInterval, 1800)
+		XCTAssertEqual(config.eventQueueLimit, 5000)
+		XCTAssertNil(config.configurationUpdateUrl)
+		XCTAssert(config.automaticallyTrackedPages.isEmpty)
+		XCTAssertEqual(config.automaticallyTracksAdvertisingId, false)
+		XCTAssertEqual(config.automaticallyTracksAppUpdates, false)
+		XCTAssertEqual(config.automaticallyTracksAppVersion, false)
+		XCTAssertEqual(config.automaticallyTracksConnectionType, false)
+		XCTAssertEqual(config.automaticallyTracksEventQueueSize, false)
+		XCTAssertEqual(config.automaticallyTracksInterfaceOrientation, false)
+	}
+}
+
+
+internal class XmlParserMinimalTest: XCTestCase {
+
+	internal var configurationData: NSData = {
+		guard let configurationFile = NSBundle(forClass: XmlParserManualTest.self).URLForResource("ConfigMinimal", withExtension: "xml") else {
+			fatalError("Cannot locate ConfigMinimal.xml")
+		}
+		guard let configurationData = NSData(contentsOfURL: configurationFile) else {
+			fatalError("Cannot load Webtrekk configuration file '\(configurationFile)'")
+		}
+		return configurationData
+	}()
+
+
+	internal func testParserInit() {
+		do {
+			try XmlTrackerConfigurationParser().parse(xml: configurationData)
+		}
+		catch let error {
+			fatalError("Cannot Read Webtrekk configuration: \(error)")
+		}
+	}
+
+	internal func testParserConfig() {
+		let config: TrackerConfiguration
+		do {
+			config = try XmlTrackerConfigurationParser().parse(xml: configurationData)
+		}
+		catch let error {
+			fatalError("Cannot Read Webtrekk configuration: \(error)")
+		}
+		XCTAssertEqual(config.version, 1)
+		XCTAssertEqual(config.serverUrl.absoluteString, "https://q3.webtrekk.net")
+		XCTAssertEqual(config.webtrekkId, "289053685367929")
+		XCTAssertEqual(config.samplingRate, 0)
+		XCTAssertEqual(config.maximumSendDelay, 5 * 60)
+		XCTAssertEqual(config.sessionTimeoutInterval, 30 * 60)
+		XCTAssertEqual(config.eventQueueLimit, 1000)
+		XCTAssertNil(config.configurationUpdateUrl)
+		XCTAssert(config.automaticallyTrackedPages.isEmpty)
+		XCTAssertEqual(config.automaticallyTracksAdvertisingId, true)
+		XCTAssertEqual(config.automaticallyTracksAppUpdates, true)
+		XCTAssertEqual(config.automaticallyTracksAppVersion, true)
+		XCTAssertEqual(config.automaticallyTracksConnectionType, true)
+		XCTAssertEqual(config.automaticallyTracksEventQueueSize, true)
+		XCTAssertEqual(config.automaticallyTracksInterfaceOrientation, true)
 	}
 }
