13ba4a5bafb4380efa8e21810b58cc1a31921a86
Benjamin Werker <benjamin@widgetlabs.eu>
2016-07-07 16:31:33 +0200

    added docu to tracker configuration

--- Sources/Public/Trackers/TrackerConfiguration.swift
+++ Sources/Public/Trackers/TrackerConfiguration.swift
@@ -3,10 +3,15 @@ import Foundation
 
 public struct TrackerConfiguration {
 
+	/** Allowed values for sendDelay */
 	public static let allowedMaximumSendDelays: ClosedInterval<NSTimeInterval> = 5 ... .infinity
+	/** Allowed values for requestQueueLimit */
 	public static let allowedRequestQueueLimits: ClosedInterval<Int> = 1 ... .max
+	/** Allowed values for samplingRate */
 	public static let allowedSamplingRates: ClosedInterval<Int> = 0 ... .max
+	/** Allowed values for sessionTimeoutInterval */
 	public static let allowedSessionTimeoutIntervals: ClosedInterval<NSTimeInterval> = 0 ... .infinity
+	/** Allowed values for version */
 	public static let allowedVersions: ClosedInterval<Int> = 1 ... .max
 
 	public var automaticallyTracksAdvertisingId = true
@@ -14,13 +19,21 @@ public struct TrackerConfiguration {
 	public var automaticallyTracksAppUpdates = true
 	public var automaticallyTracksAppVersion = true
 	public var automaticallyTracksRequestQueueSize = true
+	/** Url of the remote configuration. */
 	public var configurationUpdateUrl: NSURL? = nil
+	/** Delay after which the event request is send to the server. */
 	public var maximumSendDelay = NSTimeInterval(5 * 60)
+	/** Maxiumum number of request which are stored before sending. */
 	public var requestQueueLimit = 1000
+	/** The tracker will randomly tracks only every X user. */
 	public var samplingRate = 0
+	/** Url of the tracking server. */
 	public var serverUrl: NSURL
+	/** The timout interval indicates after a app went in the background when a new session should be tracked. */
 	public var sessionTimeoutInterval = NSTimeInterval(30 * 60)
+	/** The version is used to compare the current configuration with a remote configuration and to decide whether there is an update for the configuration available. */
 	public var version = 1
+	/** */
 	public var webtrekkId: String
 
 	#if !os(watchOS)
