6f4f9a75bddef22a73df85a218767c72afd21f7a
arsen.vartbaronov <arsen.vartbaronov@magellangps.com>
2016-09-01 13:56:49 +0200

    Changed incorrect Widgetlabs speling
    Fix is done by request from Widgetlabs
    
    [Issue #PIX-198]

--- Sources/Public/Events/Protocols/TrackingEventWithEcommerceProperties.swift
+++ Sources/Public/Events/Protocols/TrackingEventWithEcommerceProperties.swift
@@ -14,7 +14,7 @@
 //CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 //SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-//  Created by Widget Labs
+//  Created by Widgetlabs
 //
 
 public protocol TrackingEventWithEcommerceProperties: TrackingEvent {
